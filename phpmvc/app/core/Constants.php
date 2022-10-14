db900d8df1e3ca6ea5c61ac150cd6a517b1a5e68
nadianuan <nadia.nuan39@gmail.com>
2022-10-14 14:29:11 +0700

    Menyelesaikan Assets Latihan 6

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_b_203040107/phpmvc/public');
---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/avatar.jpeg" alt="Nadia Nur Annisa" width="200" class="rounded-circle shadow">
+    <p class="mt-2">Halo, nama saya <?= $data['nama']; ?>, umur saya  <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
